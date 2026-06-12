7b1c80c24c756e00930cb919766b2b74364532f1
amandubey923 <kumaraman19137@gmail.com>
2026-06-13 00:21:07 +0530

    Add user and message models, middleware for authentication and file uploads, and connect to MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import {clerkMiddleware} from "@clerk/express";
+import User from './models/user.model.js';
+import {connectDB} from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+
+app.use(express.json());
+app.use(cors({origin: FRONTEND_URL, credentials: true}));
+app.use(clerkMiddleware());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({message: "Server is healthy"});
 })
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
